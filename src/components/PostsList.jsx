d10dde451914637f0f89c86a8ed66531159d8cc0
Marco-Rocco <marcorocco28@icloud.com>
2025-04-28 09:41:38 +0200

    fix: back to old postlist and post card setting

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,15 +7,17 @@ const PostsList = () => {
 
 
     return (
-        <div className="container">
-            {posts.map((element) => (
-                <div className="post-card" key={element.id}>
-                    <h2>Titolo: {element.title}</h2>
-                    <Link to={`/${element.id}`}>{element.body}</Link>
-                    <h4>[ userId: {element.userId} ]</h4>
+        <>
+            {posts.map((post) => (
+                <div key={post.id} className="card">
+                    <div><strong>{post.title}</strong></div>
+                    <p>{post.body}</p>
+
+                    <Link to={`/post/${post.id}`}> Mostra post </Link>
                 </div>
+
             ))}
-        </div>
+        </>
     );
 };
 
